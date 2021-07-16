6ab60e270918d666b47d371bb9753dba425407c7
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-07-16 18:41:53 +0900

    Use flask

--- server/main.py
+++ server/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
