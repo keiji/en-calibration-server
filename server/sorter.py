b285bc5e6f5282bb5c45400e9d97f819deaad2b0
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-09-27 14:23:49 +0900

    Improve sort algorithm.

--- server/sorter.py
+++ server/sorter.py
@@ -1,3 +1,6 @@
+from functools import cmp_to_key
+
+
 def sort_exposure_informations(exposure_informations):
     if exposure_informations is None:
         return exposure_informations
@@ -18,12 +21,63 @@ def sort_exposure_windows(exposure_windows):
 
     for ew in exposure_windows:
         ew['ScanInstances'] = sort_scan_instances(ew['ScanInstances'])
-    return sorted(exposure_windows, key=lambda ew: ew['DateMillisSinceEpoch'])
+
+    def compare(l, r):
+        if l['DateMillisSinceEpoch'] < r['DateMillisSinceEpoch']:
+            return 1
+        if l['DateMillisSinceEpoch'] > r['DateMillisSinceEpoch']:
+            return -1
+
+        l_scan_instances_length = len(l['ScanInstances'])
+        r_scan_instances_length = len(r['ScanInstances'])
+        if l_scan_instances_length < r_scan_instances_length:
+            return 1
+        if l_scan_instances_length > r_scan_instances_length:
+            return -1
+
+        l_total_scan_instances_min_attenuation_db = sum(map(lambda si: si['MinAttenuationDb'], l['ScanInstances']))
+        r_total_scan_instances_min_attenuation_db = sum(map(lambda si: si['MinAttenuationDb'], r['ScanInstances']))
+        if l_total_scan_instances_min_attenuation_db < r_total_scan_instances_min_attenuation_db:
+            return 1
+        if l_total_scan_instances_min_attenuation_db > r_total_scan_instances_min_attenuation_db:
+            return -1
+
+        l_typical_attenuationDb = sum(map(lambda si: si['TypicalAttenuationDb'], l['ScanInstances']))
+        r_typical_attenuationDb = sum(map(lambda si: si['TypicalAttenuationDb'], r['ScanInstances']))
+        if l_typical_attenuationDb < r_typical_attenuationDb:
+            return 1
+        if l_typical_attenuationDb > r_typical_attenuationDb:
+            return -1
+
+        l_seconds_since_last_scan = sum(map(lambda si: si['SecondsSinceLastScan'], l['ScanInstances']))
+        r_seconds_since_last_scan = sum(map(lambda si: si['SecondsSinceLastScan'], r['ScanInstances']))
+        if l_seconds_since_last_scan < r_seconds_since_last_scan:
+            return 1
+        if l_seconds_since_last_scan > r_seconds_since_last_scan:
+            return -1
+
+        return 0
+
+    return sorted(exposure_windows, key=cmp_to_key(compare))
 
 
 def sort_scan_instances(scan_instances):
     if scan_instances is None:
         return scan_instances
 
-    return sorted(scan_instances,
-                  key=lambda si: (si['MinAttenuationDb'] + si['SecondsSinceLastScan'] + si['TypicalAttenuationDb']))
+    def compare(l, r):
+        if l['MinAttenuationDb'] < r['MinAttenuationDb']:
+            return 1
+        if l['MinAttenuationDb'] > r['MinAttenuationDb']:
+            return -1
+        if l['SecondsSinceLastScan'] < r['SecondsSinceLastScan']:
+            return 1
+        if l['SecondsSinceLastScan'] > r['SecondsSinceLastScan']:
+            return -1
+        if l['TypicalAttenuationDb'] < r['TypicalAttenuationDb']:
+            return 1
+        if l['TypicalAttenuationDb'] > r['TypicalAttenuationDb']:
+            return -1
+        return 0
+
+    return sorted(scan_instances, key=cmp_to_key(compare))
