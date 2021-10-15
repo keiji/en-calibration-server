714a9d8bb2ce018c89b2c5fcc2961a2f611ed8e4
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-10-15 17:39:58 +0900

    Implement showing datetime and output csv file.

--- server/web_api.py
+++ server/web_api.py
@@ -1,9 +1,10 @@
 import hashlib
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from http import HTTPStatus
 import uuid
+import csv
 
 from flask import Flask, send_file, request, Response
 from sqlalchemy import create_engine
@@ -13,6 +14,8 @@ from common import convert_to_diagnosis_key, is_exists, FORMAT_RFC3339
 from scheme import Base
 from configuration import Configuration
 from sorter import sort_daily_summaries, sort_exposure_windows, sort_exposure_informations
+
+JST = timezone(timedelta(hours=9), 'Asia/Tokyo')
 
 DIAGNOSIS_KEYS_DIR = 'diagnosis_keys'
 EXPOSURE_DATA_DIR = 'exposure_data'
@@ -64,6 +67,7 @@ def _is_valid_cluster_id(cluster_id):
 
 MIMETYPE_JSON = 'application/json'
 MIMETYPE_ZIP = 'application/zip'
+MIMETYPE_CSV = 'text/csv'
 
 
 @app.route("/diagnosis_keys/<cluster_id>/list.json", methods=['GET'])
@@ -82,8 +86,14 @@ def diagnosis_keys_index(cluster_id):
     for file_name in filtered_zip_list:
         url = os.path.join(config.base_url, DIAGNOSIS_KEYS_DIR, cluster_id, file_name)
         path = os.path.join(config.base_path, cluster_id, DIAGNOSIS_KEYS_DIR, file_name)
-        timestamp = int(os.stat(path).st_mtime)
-        item = {'region': config.region, 'url': url, 'created': timestamp}
+        created_timestamp = os.stat(path).st_mtime
+        created_datetime = datetime.fromtimestamp(created_timestamp).astimezone(JST)
+        item = {
+            'region': config.region,
+            'url': url,
+            'created': int(created_timestamp),
+            'datetime': created_datetime.strftime(FORMAT_RFC3339)
+        }
         item_list.append(item)
     json_str = json.dumps(item_list, indent=4)
 
@@ -166,10 +176,22 @@ def exposure_data_index(cluster_id):
     item_list = []
 
     for file_name in filtered_json_list:
-        url = os.path.join(config.base_url, EXPOSURE_DATA_DIR, cluster_id, file_name)
+        identifier, _ = os.path.splitext(file_name)
+        json_url = os.path.join(config.base_url, EXPOSURE_DATA_DIR, cluster_id, file_name)
+        exposure_windows_csv_url = os.path.join(config.base_url, EXPOSURE_DATA_DIR, cluster_id, identifier,
+                                                "exposure_windows.csv")
+        daily_summaries_csv_url = os.path.join(config.base_url, EXPOSURE_DATA_DIR, cluster_id, identifier,
+                                               "daily_summaries.csv")
         path = os.path.join(config.base_path, cluster_id, EXPOSURE_DATA_DIR, file_name)
-        timestamp = int(os.stat(path).st_mtime)
-        item = {'url': url, 'created': timestamp}
+        created_timestamp = os.stat(path).st_mtime
+        created_datetime = datetime.fromtimestamp(created_timestamp).astimezone(JST)
+        item = {
+            'url': json_url,
+            'exposure_windows_csv_url': exposure_windows_csv_url,
+            'daily_summaries_csv_url': daily_summaries_csv_url,
+            'created': int(created_timestamp),
+            'datetime': created_datetime.strftime(FORMAT_RFC3339)
+        }
         item_list.append(item)
 
     item_list = sorted(item_list, key=lambda item: item['created'], reverse=True)
@@ -201,6 +223,95 @@ def exposure_data(cluster_id, file_name):
         )
 
 
+def _convert_exposure_windows_to_csv(cluster_id, identifier, json_obj):
+    exposure_windows = json_obj['exposure_windows']
+
+    if exposure_windows is None:
+        return "", HTTPStatus.NOT_FOUND
+
+    file_path = os.path.join(config.base_path, cluster_id, EXPOSURE_DATA_DIR, "exposure_windows-%s.csv" % identifier)
+    if not os.path.exists(file_path):
+        with open(file_path, mode='w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(
+                [
+                    "CalibrationConfidence", "DateMillisSinceEpoch", "WindowNumber", "Infectiousness", "ReportType",
+                    "MinAttenuationDb", "SecondsSinceLastScan", "TypicalAttenuationDb"
+                ])
+
+            for window_number, ew in enumerate(exposure_windows):
+                calibrationConfidence = ew["CalibrationConfidence"]
+                dateMillisSinceEpoch = ew["DateMillisSinceEpoch"]
+                infectiousness = ew["Infectiousness"]
+                reportType = ew["ReportType"]
+                for si in ew["ScanInstances"]:
+                    writer.writerow([
+                        calibrationConfidence, dateMillisSinceEpoch, window_number, infectiousness, reportType,
+                        si["MinAttenuationDb"], si["SecondsSinceLastScan"], si["TypicalAttenuationDb"],
+                    ])
+
+    with open(file_path) as fp:
+        return Response(response=fp.read(),
+                        status=HTTPStatus.OK,
+                        mimetype=MIMETYPE_CSV)
+
+
+def _write_csv_row(dateMillisSinceEpoch, daily_summary, type, csv_writer):
+    summary = daily_summary[type]
+    if summary is not None:
+        csv_writer.writerow([
+            dateMillisSinceEpoch,
+            type,
+            summary["MaximumScore"],
+            summary["ScoreSum"],
+            summary["WeightedDurationSum"]
+        ])
+
+
+def _convert_daily_summaries_to_csv(cluster_id, identifier, json_obj):
+    daily_summaries = json_obj['daily_summaries']
+
+    if daily_summaries is None:
+        return "", HTTPStatus.NOT_FOUND
+
+    file_path = os.path.join(config.base_path, cluster_id, EXPOSURE_DATA_DIR, "daily_summaries-%s.csv" % identifier)
+    if not os.path.exists(file_path):
+        with open(file_path, mode='w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(["DateMillisSinceEpoch", "Type", "MaximumScore", "ScoreSum", "WeightedDurationSum"])
+
+            for ds in daily_summaries:
+                dateMillisSinceEpoch = ds["DateMillisSinceEpoch"]
+                _write_csv_row(dateMillisSinceEpoch, ds, "DaySummary", writer)
+                _write_csv_row(dateMillisSinceEpoch, ds, "ConfirmedClinicalDiagnosisSummary", writer)
+                _write_csv_row(dateMillisSinceEpoch, ds, "ConfirmedTestSummary", writer)
+                _write_csv_row(dateMillisSinceEpoch, ds, "RecursiveSummary", writer)
+                _write_csv_row(dateMillisSinceEpoch, ds, "SelfReportedSummary", writer)
+
+    with open(file_path) as fp:
+        return Response(response=fp.read(),
+                        status=HTTPStatus.OK,
+                        mimetype=MIMETYPE_CSV)
+
+
+@app.route("/exposure_data/<cluster_id>/<identifier>/<type>", methods=['GET'])
+def exposure_data_detail(cluster_id, identifier, type):
+    if not _is_valid_cluster_id(cluster_id):
+        return "ClusterID:%s invalid" % cluster_id
+
+    file_path = os.path.join(config.base_path, cluster_id, EXPOSURE_DATA_DIR, "%s.json" % identifier)
+    if not os.path.exists(file_path):
+        return "", HTTPStatus.NOT_FOUND
+
+    with open(file_path, 'r') as fp:
+        json_obj = json.load(fp)
+
+        if type == 'exposure_windows.csv':
+            return _convert_exposure_windows_to_csv(cluster_id, identifier, json_obj)
+        elif type == 'daily_summaries.csv':
+            return _convert_daily_summaries_to_csv(cluster_id, identifier, json_obj)
+
+
 def _is_valid_exposure_data(exposure_data):
     if not 'en_version' in exposure_data:
         return False
@@ -216,7 +327,7 @@ def _is_valid_exposure_data(exposure_data):
     return True
 
 
-def _get_file_name(json_obj):
+def _get_identifier(json_obj):
     json_str = json.dumps(json_obj)
     sha256 = hashlib.sha256()
     sha256.update(json_str.encode('UTF-8'))
@@ -254,7 +365,8 @@ def put_exposure_data(cluster_id):
     output_dir = os.path.join(config.base_path, str(cluster_id), EXPOSURE_DATA_DIR)
     os.makedirs(output_dir, exist_ok=True)
 
-    file_name = _get_file_name(json_obj)
+    identifier = _get_identifier(json_obj)
+    file_name = "%s.json" % identifier
 
     json_obj['file_name'] = file_name
     json_obj['url'] = os.path.join(config.base_url, EXPOSURE_DATA_DIR, cluster_id, file_name)
